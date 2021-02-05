02a91777038872001e2ba35ad4e445965326f577
Satyam <satyambhardawaj@gmail.com>
2021-02-05 11:57:55 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const userRouter = require('../routers/user')
 require('./db')
+const cors = require('cors')
 
 const app = express()
 
-
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 
